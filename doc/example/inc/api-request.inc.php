40709b67c020c3c6bf25cc5e14a2e88fbf3ed5bf
Stefan <stefan@aptoma.com>
2015-01-30 16:07:29 +0100

    updated documentation

--- doc/example/inc/api-request.inc.php
+++ doc/example/inc/api-request.inc.php
@@ -12,12 +12,11 @@ if (strpos($url, '?') !== false) {
 <div class="request-info">
     <?php // "'$searchServerUrl'"?>
     API request parameters: <input type="text" class="api-request" value="<?php print($params) ?>" /><br/>
-    API request URL: <a href="<?php print($url) ?>" target="_blank"><?php print($url) ?></a><br/>
-    Search server URL: <a href="<?php print(str_replace('"', '%22', $searchServerUrl )) ?>" target="_blank"><?php print ($searchServerUrl) ?></a>
+    Entire API request URL: <a href="<?php print($url) ?>" target="_blank"><?php print($url) ?></a><br/>
 
 <div>
-    <strong>Process time: <span id="procTime"></span> sec</strong> [curl request: <?php print(round($curlTime,3)) ?> sec, processing and rendering: <span id="procRendTime"></span>],
-    download speed: <?php print($curlDLSpeed) ?> MBit/s, data transfered: <?php print($curlDLSize) ?> MB
+    <strong>Processing time: <span id="procTime"></span> sec</strong> [curl request: <?php print(round($curlTime,3)) ?> sec, processing and rendering: <span id="procRendTime"></span>],
+    download speed: <?php print($curlDLSpeed) ?> MBit/s, data transferred: <?php print($curlDLSize) ?> MB
 </div>
 </div>
 
